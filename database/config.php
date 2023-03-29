cf429f7e0861bd3317c4f3b54471d28fcf9fe2e1
MiloszZiemniak <ziemiaaak@gmail.com>
2023-03-29 23:19:15 +0200

    Działająca wersja

---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+//database configurations
+define("DB_HOST","localhost");
+define("DB_UNAME","root");
+define("DB_PASS","1234");
+define("DB_DNAME","testowabd");
+$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
+?>
